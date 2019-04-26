935bef8670ab76335ecf175f09db4c94c3cf18e2
James Pak <jamesjiminpak@gmail.com>
2019-04-26 11:53:55 -0700

    styling updated

--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -3,18 +3,21 @@ import React from "react";
 function Character(props) {
   return (
     <div className="character-card">
-      
       <div className="character-info">
         <h1>{props.character.name}</h1>
-        <p>
-          <strong>Born in:</strong> {props.character.birth_year}
-        </p>
-        <p>
-          <strong>Eye Color:</strong> {props.character.eye_color}
-        </p>
-        <p>
-          <strong>Gender:</strong> {props.character.gender}
-        </p>
+       
+       <div className = "list-style">
+          <li>
+            <strong>Born in:</strong> {props.character.birth_year}
+          </li>
+          <li>
+            <strong>Eye Color:</strong> {props.character.eye_color}
+          </li>
+          <li>
+            <strong>Gender:</strong> {props.character.gender}
+          </li>
+        </div>
+       
       </div>
     </div>
   );
